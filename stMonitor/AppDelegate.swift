f0cf9fc4bb387af31a6a3adb4d2471260ae311d0
Yaroslav <akimaleo@ya.ru>
2017-07-16 00:19:59 +0300

    Initial Commit

---
+++ stMonitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  stMonitor
+//
+//  Created by Yaroslav Movchan on 16.07.17.
+//  Copyright © 2017 letit0or1. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ stMonitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  stMonitor
+//
+//  Created by Yaroslav Movchan on 16.07.17.
+//  Copyright © 2017 letit0or1. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
